9557da80b6698ace11e22e0a2696acb0284e36ba
lkim016 <kim.lori57@gmail.com>
2025-12-04 18:20:19 -0800

    checking issue with server msg

--- src/server.c
+++ src/server.c
@@ -34,7 +34,7 @@ void rcv_socket(int socket, int client_sock) {
   //-----------
   // Respond to client:
   strcpy(server_message, "Server is processing sent message...");
-  
+  printf("%s\n", server_message); // check
   if (send(client_sock, server_message, strlen(server_message), 0) < 0){
     printf("Can't send\n");
     close(socket);
